b7d19a6e2e1c50f5b3f8b669747a36b59c11a3f9
Sabsaunter <83870275+Sabsaunter@users.noreply.github.com>
2021-06-25 18:13:36 +1000

    Delete src directory

--- src/main/java/week3/day2/HardWare.java
+++
@@ -1,7 +0,0 @@
-package week3.day2;
-
-public interface HardWare {
-	
-	void hardwareResources();
-
-}
--- src/test/java/runners/RunTest.java
+++
@@ -1,9 +0,0 @@
-package runners;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/test/java/features/Login.feature", glue= {"steps","hooks"}, monochrome = true, publish = true, tags="@smoke")
-public class RunTest extends AbstractTestNGCucumberTests{
-
-}
